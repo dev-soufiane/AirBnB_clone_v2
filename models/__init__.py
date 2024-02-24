cdf4d36713654f302e73179bae9519c59af2d45b
dev-soufiane <dev.sofyane@gmail.com>
2024-02-25 00:55:45 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,13 @@
 """
 This module initializes the package and sets up the storage.
 """
-
 from os import getenv
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
 
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
