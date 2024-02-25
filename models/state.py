0f46fcaed17c788042530b56cb2b944d5b7f6700
dev-soufiane <dev.sofyane@gmail.com>
2024-02-25 01:16:02 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This module defines the State class."""
-
-from os import getenv
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
 import models
-from models.base_model import BaseModel, Base
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
