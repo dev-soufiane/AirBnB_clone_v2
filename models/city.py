aef0d0f9bbb466213ae5fa80bca425d9ed01e877
dev-soufiane <dev.sofyane@gmail.com>
2024-02-25 01:13:28 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This module defines the City class."""
-
-from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """Class that represents a city for the application."""
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', backref='cities',
-                          cascade='all, delete-orphan')
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
