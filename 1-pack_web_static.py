521f7af0cb6dd8f8f6de91f6f9213b0c302395a8
dev-soufiane <dev.sofyane@gmail.com>
2024-03-10 23:40:35 +0000

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""
-Generates a .tgz archive from the contents of the web_static folder.
-"""
-
-from fabric.operations import local as fabric_local
-from datetime import datetime as dt
+from fabric.api import local as fabric_local
+from time import strftime as fmt_time
+from datetime import date as dt
 
 def do_pack():
-    """Function to compress files"""
-    fabric_local("mkdir -p versions")
-    result = fabric_local("tar -cvzf versions/web_static_{}.tgz web_static"
-                          .format(dt.strftime(dt.now(), "%Y%m%d%H%M%S")),
-                          capture=True)
-    if result.failed:
+    """Generates and archives the contents of the web_static folder."""
+
+    timestamp = fmt_time("%Y%m%d%H%M%S")
+    try:
+        fabric_local("mkdir -p versions")
+        fabric_local("tar -czvf versions/web_static_{}.tgz web_static/"
+                     .format(timestamp))
+
+        return "versions/web_static_{}.tgz".format(timestamp)
+
+    except Exception as e:
         return None
-    return result
